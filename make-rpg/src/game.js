6d3abc78caf26e415e3e6f3b27f04cf09a9c1bfb
teramotodaiki <i101323@gmail.com>
2018-04-20 16:41:59 +0900

    feature: goal point as an asset

--- make-rpg/src/game.js
+++ make-rpg/src/game.js
@@ -9,16 +9,6 @@ async function gameFunc() {
 	player.family = ('▼ ファミリー', Family.プレイヤー);
 	player.locate(3, 5); // はじめの位置
 
-	Hack.log('おしろがみえるだろう あれがゴールだ'); // メッセージを出す
-
-	const item1 = new RPGObject(('▼ スキン', Skin.キャッスル));
-	item1.locate(10, 5, 'map1');
-	item1.on(('▼ イベント', 'のった'), () => {
-		Hack.gameclear(); // ゲームクリア
-		player.destroy(); // プレイヤーをけす
-		Hack.log('ゲームクリアです。おめでとう！'); // メッセージを出す
-	});
-
 	/*+ モンスター アイテム せっち システム */
 
 	/*+ スキル */
