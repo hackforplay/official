a15ca50570bcd73dada0849227da5ed6832bc8ce
teramotodaiki <i101323@gmail.com>
2018-04-21 17:46:52 +0900

    feature: test madosho code

--- rockman-proto/src/code.js
+++ rockman-proto/src/code.js
@@ -1,2 +1,15 @@
-// ゲームクリア
-Hack.gameclear();
+let _x = 5,
+	_y = 5;
+class MyRockman extends Rockman {
+	しょうかんされたら() {
+		Hack.log('こんにちは！');
+	}
+	プレイヤーがあるいたら(x, y) {
+		this.locate(_x, _y);
+		_x = x;
+		_y = y;
+	}
+}
+
+// ロックマンをしょうかん！
+summonRockman(MyRockman);
