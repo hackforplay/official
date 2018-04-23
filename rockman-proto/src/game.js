04692ea336ba00c2af3c22a3e4dd94e6fe210a79
teramotodaiki <i101323@gmail.com>
2018-04-23 16:22:10 +0900

    feature: demo - attack to Metall

--- rockman-proto/src/game.js
+++ rockman-proto/src/game.js
@@ -1,6 +1,7 @@
 import 'hackforplay/core';
 // import 'mod/3d/core';
 import './mod/rockman/index';
+import './resources/preload';
 
 async function gameFunc() {
 	Hack.changeMap('map1'); // map1 をロード
@@ -10,6 +11,11 @@ async function gameFunc() {
 	player.locate(3, 5); // はじめの位置
 
 	feeles.openCode('code.js');
+
+	const item1 = new RPGObject(Skin.ネオメットール);
+	item1.locate(10, 5);
+	item1.hp = 1;
+	item1.turn();
 
 	/*+ モンスター アイテム せっち システム */
 
