1d59f7cbac1732ac5eae83768705bcffb3a4895e
teramotodaiki <i101323@gmail.com>
2018-05-18 13:20:03 +0900

    dev: enable cors request

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const port = process.env.PORT || 3000;
 
 app.use(require('morgan')('dev'));
 app.use(require('compression')());
+app.use(require('cors')());
+
 app.use(require('serve-static')(__dirname + '/public'));
 
 app.listen(port, function() {
