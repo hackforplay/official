c68901169d6618cc075710de88feff57029a2ead
teramotodaiki <i101323@gmail.com>
2018-05-23 15:22:30 +0900

    feature: addtrodden を Walk から Idle に移った時だけ起こるイベントに仕様変更

--- common/hackforplay/trodden.js
+++ common/hackforplay/trodden.js
@@ -2,46 +2,95 @@ import SAT from '../lib/sat.min';
 import enchant from '../enchantjs/enchant';
 import './enchantjs-kit';
 import RPGObject from './object/object';
+import BehaviorTypes from './behavior-types';
 
 const game = enchant.Core.instance;
 game.on('enterframe', trodden);
 
+const walkingRPGObjects = new WeakSet();
+const targetItemSetMap = new WeakMap();
+
 /**
- * isKinematic (velocity, force がない) な RPGObject の collider に
- * 任意の RPGObject の center が入ったときに "trodden" イベントを dispatch する
+ * 1. 任意の RPGObject(item) が Walk から Idle になった (歩き終えた) とき,
+ * 2. もし (item) の center がある別の RPGObject(target) の colliders の
+ *    いずれかに含まれていたとき, (target) に "addtrodden" イベントを発火する
+ * 3. その後, (item) が (target) から離れたとき (target) に "removetrodden" イベントを発火する
+ * NOTICE: "removetrodden" イベント中に locate で移動すると, うまく移動できない
  */
 export default function trodden() {
 	const { collection } = RPGObject;
-	const kinematics = collection.filter(item => item.isKinematic);
-	for (const target of kinematics) {
-		const colliders = target.colliders || [target.collider];
-		target._currentTroddenObjects = target._currentTroddenObjects || new Set();
-		// さっきまで踏んでいたオブジェクトが今も残っているか調べる
-		// オブジェクトは collection から削除されている可能性があることに注意する
-		for (const item of target._currentTroddenObjects) {
-			if (!isTrodden(colliders, item.center)) {
-				dispatch('removetrodden', target, item);
-				target._currentTroddenObjects.delete(item);
+
+	// 2
+	for (const item of RPGObject.collection) {
+		if (walkingRPGObjects.has(item) && item.behavior === BehaviorTypes.Idle) {
+			for (const target of RPGObject.collection) {
+				if (isTrodden(target, item)) {
+					dispatch('addtrodden', target, item);
+					if (!targetItemSetMap.has(target)) {
+						const itemSet = new Set();
+						itemSet.add(item);
+						targetItemSetMap.set(target, itemSet);
+					} else {
+						const itemSet = targetItemSetMap.get(target);
+						itemSet.add(item);
+					}
+				}
 			}
 		}
-		for (const item of collection) {
-			if (target === item) continue;
-			if (
-				!target._currentTroddenObjects.has(item) &&
-				isTrodden(colliders, item.center)
-			) {
-				dispatch('addtrodden', target, item);
-				target._currentTroddenObjects.add(item);
+	}
+
+	// 3
+	// さっきまで踏んでいたオブジェクトが今も残っているか調べる
+	// オブジェクトは collection から削除されている可能性があることに注意する
+	for (const target of RPGObject.collection) {
+		if (targetItemSetMap.has(target)) {
+			const itemSet = targetItemSetMap.get(target);
+			for (const item of new Set(itemSet)) {
+				if (!isTrodden(target, item)) {
+					dispatch('removetrodden', target, item);
+					itemSet.delete(item);
+				}
 			}
+			if (itemSet.size < 1) {
+				targetItemSetMap.delete(target);
+			}
+		}
+	}
+
+	// 1
+	for (const item of RPGObject.collection) {
+		if (item.behavior === BehaviorTypes.Walk) {
+			walkingRPGObjects.add(item);
+		} else {
+			walkingRPGObjects.delete(item);
 		}
 	}
 }
 
-function isTrodden(colliders, center) {
-	const p = new SAT.Vector(center.x, center.y);
+/**
+ * colliders を item が踏んでいるかどうかを調べる
+ * @param {RPGObject} target ふまれるかも知れないオブジェクトのコライダー
+ * @param {RPGObject} item ふむかも知れないオブジェクト
+ */
+function isTrodden(target, item) {
+	if (
+		target === item ||
+		!RPGObject.collection.includes(target) ||
+		!RPGObject.collection.includes(item)
+	) {
+		return false;
+	}
+	const colliders = target.colliders || [target.collider];
+	const p = new SAT.Vector(item.center.x, item.center.y);
 	return colliders.some(poly => SAT.pointInPolygon(p, poly));
 }
 
+/**
+ * addtrodden または removetrodden イベントを発火させる
+ * @param {String} name イベント名
+ * @param {RPGObject} target ふまれたオブジェクト
+ * @param {RPGObject} item ふんだオブジェクト
+ */
 function dispatch(name, target, item) {
 	const event = new Event(name);
 	event.item = item;
