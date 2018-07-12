e2a1824d1d841884ef2bdabd6d9aaae15daa5f3e
teramotodaiki <i101323@gmail.com>
2018-07-12 22:21:55 +0900

    feature: createCompatibleMap の実装とテスト

--- test/create-compatible-map.spec.js
+++ test/create-compatible-map.spec.js
@@ -1,6 +1,18 @@
 import test from 'ava';
+import fs from 'fs';
+import path from 'path';
 import createCompatibleMap from '../common/hackforplay/create-compatible-map';
 import enchant from './helpers/enchant';
+import createMapJson from './helpers/create-map-json';
+import { Image } from 'canvas-prebuilt';
+import RPGMap from './helpers/mock-rpg-map';
+
+// テストのための依存注入
+const injection = {
+	RPGMap,
+	Surface: enchant.Surface,
+	Image
+};
 
 test('create new instance from enchant.Game', t => {
 	enchant();
@@ -9,145 +21,63 @@ test('create new instance from enchant.Game', t => {
 	t.is(core.height, 320);
 });
 
-// Data URL
-const dataURL = {
-	transparent:
-		'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAXNSR0IArs4c6QAAAChJREFUWAnt0IEAAAAAw6D5Ux/khVBhwIABAwYMGDBgwIABAwYMvA8MECAAAc4qtccAAAAASUVORK5CYII=',
-	white:
-		'data:image/png,base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAXNSR0IArs4c6QAAADJJREFUWAnt0EENAAAIAzHAv2cgmODTGbil2bt4XD22L+0AAQIECBAgQIAAAQIECBAgME2IBDzy317fAAAAAElFTkSuQmCC',
-	black:
-		'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAXNSR0IArs4c6QAAADVJREFUWAnt0LENAEAIAkD8/XdWi9/B5kioIVdJenuWd7b8hx0gQIAAAQIECBAgQIAAAQIEBiESAT/N9/smAAAAAElFTkSuQmCC'
-};
+let result;
 
-test('Hack.createCompatibleMap', t => {
-	const exampleMap = {
-		tables: [
-			[
-				[101, 101, 101, 101, 101, 101, 101, 101, 101, 101],
-				[101, 101, 101, 101, 101, 101, 101, 101, 101, 101],
-				[101, 101, 101, 101, 101, 101, 101, 101, 101, 101],
-				[101, 101, 101, 101, 101, 101, 101, 101, 101, 101],
-				[101, 101, 101, 101, 101, 101, 101, 101, 101, 101],
-				[101, 101, 101, 101, 101, 101, 101, 101, 101, 101]
-			],
-			[
-				[100, 100, 100, 100, 100, 100, 100, 100, 100, 100],
-				[100, 100, 100, 100, 100, 100, 100, 100, 100, 100],
-				[100, 100, 100, 102, 102, 102, 102, 100, 100, 100],
-				[100, 100, 100, 102, 102, 102, 102, 100, 100, 100],
-				[100, 100, 100, 100, 100, 100, 100, 100, 100, 100],
-				[100, 100, 100, 100, 100, 100, 100, 100, 100, 100]
-			],
-			[
-				[100, 100, 100, 100, 100, 100, 100, 100, 100, 102],
-				[100, 100, 103, 103, 100, 100, 100, 100, 100, 102],
-				[100, 100, 103, 103, 100, 100, 100, 100, 100, 102],
-				[100, 100, 103, 103, 100, 100, 100, 100, 100, 102],
-				[100, 100, 100, 100, 100, 100, 100, 100, 100, 102],
-				[100, 100, 100, 100, 100, 100, 100, 100, 100, 102]
-			]
-		],
-		squares: {
-			'100': {
-				tile: {
-					size: [32, 32],
-					image: {
-						type: 'data-url',
-						value: dataURL.transparent
-					},
-					order: 'Inherit',
-					isAutoTile: false,
-					isAnimation: false,
-					collider: [false, false, false, false],
-					authors: []
-				},
-				order: 'Inherit',
-				collider: [false, false, false, false]
-			},
-			'101': {
-				tile: {
-					size: [32, 32],
-					image: {
-						type: 'data-url',
-						value: dataURL.black
-					},
-					order: 'Inherit',
-					isAutoTile: false,
-					isAnimation: false,
-					collider: [false, false, false, false],
-					authors: []
-				},
-				order: 'Inherit',
-				collider: [false, false, false, false]
-			},
-			'102': {
-				tile: {
-					size: [32, 32],
-					image: {
-						type: 'data-url',
-						value: dataURL.white
-					},
-					order: 'Object',
-					isAutoTile: false,
-					isAnimation: false,
-					collider: [true, true, true, true],
-					authors: []
-				},
-				order: 'Object',
-				collider: [true, true, true, true]
-			},
-			'103': {
-				tile: {
-					size: [32, 32],
-					image: {
-						type: 'data-url',
-						value: dataURL.black
-					},
-					order: 'Above',
-					isAutoTile: false,
-					isAnimation: false,
-					collider: [false, false, false, false],
-					authors: []
-				},
-				order: 'Above',
-				collider: [false, false, false, false]
-			}
-		}
-	};
+test.cb('Hack.createCompatibleMap', t => {
+	const exampleMap = createMapJson();
 
-	const result = createCompatibleMap(exampleMap, {
-		RPGMap,
-		Surface: enchant.Surface
-	});
+	result = createCompatibleMap(exampleMap, injection, t.end);
 
 	t.true(result instanceof RPGMap);
 	if (!(result instanceof RPGMap)) return;
-	t.is(
-		result.image.width * result.image.height,
-		32 * 32 * 4,
-		'32x32のタイル4枚分のバッファが必要です'
-	);
+	t.truthy(result.image._element, 'Canvas が作られていません');
+	t.true(result.image.width * result.image.height > 0, 'バッファが空です');
+	t.is(result.bmap.width, 10 * 32);
+	t.is(result.bmap.height, 6 * 32);
+	t.is(result.fmap.width, 10 * 32);
+	t.is(result.fmap.height, 6 * 32);
 	t.false(result.hitTest(0, 0), 'すべてのテーブルで通路です');
-	t.false(result.hitTest(3 * 32, 2 * 32), '壁を通路で上書きします');
+	t.true(result.hitTest(3 * 32, 2 * 32), '屋根のひさし on 岩');
 	t.true(result.hitTest(4 * 32, 2 * 32), '継承されても壁のままです');
 	t.true(result.hitTest(9 * 32, 5 * 32), '最も手前のタイルが壁です');
-	t.deepEqual(result.bmap.data, [
+	t.deepEqual(
+		result.cmap,
 		[
-			/*データ*/
-		]
-	]);
-	t.deepEqual(result.fmap.data, [
-		[
-			/*データ*/
-		]
-	]);
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+			[0, 0, 0, 1, 1, 1, 1, 0, 0, 1],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+		],
+		'cmap'
+	);
+	t.true(Array.isArray(result.bmap._data[0]));
+	t.true(Array.isArray(result.bmap._data[0][0]));
+	t.is(typeof result.bmap._data[0][0][0], 'number');
+	t.true(Array.isArray(result.fmap._data[0]));
+	t.true(Array.isArray(result.fmap._data[0][0]));
+	t.is(typeof result.fmap._data[0][0][0], 'number');
 });
 
-class RPGMap {
-	constructor() {
-		this.bmap = new enchant.Map();
-		this.fmap = new enchant.Map();
-		this.image = new enchant.Surface();
-	}
-	hitTest(x, y) {}
-}
+const tmp = path.join(__dirname, 'tmp');
+
+test.after.cb('save buffer image of RPGMap::image', t => {
+	const dataURL = result.image._element.toDataURL(); // default png
+	const [, base64] = dataURL.split(',');
+	fs.writeFile(path.join(tmp, 'image.png'), base64, 'base64', t.end);
+});
+
+test.after.cb('save rendered image of RPGMap::bmap', t => {
+	result.bmap.redraw(0, 0, 10 * 32, 6 * 32);
+	const dataURL = result.bmap._surface._element.toDataURL();
+	const [, base64] = dataURL.split(',');
+	fs.writeFile(path.join(tmp, 'bmap.png'), base64, 'base64', t.end);
+});
+
+test.after.cb('save rendered image of RPGMap::fmap', t => {
+	result.fmap.redraw(0, 0, 10 * 32, 6 * 32);
+	const dataURL = result.fmap._surface._element.toDataURL();
+	const [, base64] = dataURL.split(',');
+	fs.writeFile(path.join(tmp, 'fmap.png'), base64, 'base64', t.end);
+});
