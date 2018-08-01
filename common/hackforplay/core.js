78cbd17ac6fd51c9f98a0dcfb2bdf5768446d581
teramotodaiki <i101323@gmail.com>
2018-08-02 08:12:40 +0900

    refactoring: mod のファイルを移動

--- common/hackforplay/core.js
+++ common/hackforplay/core.js
@@ -8,7 +8,7 @@ import Hack from './hack';
 import * as synonyms from './synonyms';
 import Skin from './skin';
 import Family from './family';
-import './mod/collider-debugger';
+import '../mod/collider-debugger';
 import stopOnBlur from '../mod/stop-on-blur';
 import * as logFunc from '../mod/logFunc';
 import RPGObject from './object/object';
