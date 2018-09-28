b356053178f087cf967ef5b02f7614b1915973d0
teramotodaiki <i101323@gmail.com>
2018-09-28 23:22:42 +0900

    feature: hack, make を common@0.8 にアップデート, リソースパスを修正

--- make-rpg/main.js
+++ make-rpg/main.js
@@ -2,7 +2,7 @@ import {
 	enchant,
 	Hack,
 	register
-} from 'https://unpkg.com/@hackforplay/common@^0.7';
+} from 'https://unpkg.com/@hackforplay/common@^0.8';
 
 import gameFunc from './game';
 import maps from './maps';
