fe0cd78395f9ad69d783b9b4e503e1aa2dca1ad1
teramotodaiki <i101323@gmail.com>
2018-10-01 15:42:39 +0900

    feature: ゲームのレベルデザインができる関数を追加

--- typing/game.js
+++ typing/game.js
@@ -1,88 +1,151 @@
 const odaihyou = [
-	// Lv.0
+	// level 0
 	'FJ',
-	// Lv.1
+	// level 1
 	'FGHJ',
-	// Lv.2
+	// level 2
 	'ASDFGHJKL',
-	// Lv.3
+	// level 3
 	'ZXCVBNM',
-	// Lv.4
+	// level 4
 	'QWERTYUIOP',
-	// Lv.5
+	// level 5^
 	'QWERTYUIOPASDFGHJKLZXCVBNM'
 ];
-const mozisu = 3;
 const kugiri = ' ';
 
+const level = () => {
+	return Math.floor(Hack.score / 20);
+};
+
+const mozisu = () => {
+	const lv = level();
+	if (lv === 0) return 3;
+	if (lv === 1) return 4;
+	if (lv === 2) return 5;
+	if (lv === 3) return 6;
+	if (lv === 4) return 7;
+	else return 8;
+};
+
+const hp = () => {
+	return 1 + level();
+};
 
 async function gameFunc() {
 	Hack.changeMap('map1'); // map1 をロード
 	
-	// ラベルを消す
+	// ラベル/UIを消す
 	Hack.lifeLabel.parentNode.removeChild(Hack.lifeLabel);
-	// COUNT LABEL
-	let count = 0; // 入力した回数
-	Hack.scoreLabel.label = 'COUNT:';
-	Hack.scoreLabel.onenterframe = () => {
-		Hack.scoreLabel.score = count;
-	};
+	Hack.pad.parentNode.removeChild(Hack.pad);
+	Hack.apad.parentNode.removeChild(Hack.apad);
+	
+	Hack.scoreLabel.y = 120;
 	
 	self.player = new Player(('▼ スキン', Skin.ナイト)); // プレイヤーをつくる
 	player.name = 'プレイヤー';
 	player.family = ('▼ ファミリー', Family.プレイヤー);
 	player.locate(3, 5); // はじめの位置
-	player.hp = 3; // 体力
+	player.hp = 1; // 体力
 	player.atk = 1; // こうげき力
+	player.forward = [1, 0];
 	
-	const t = new TextArea(480, 90);
-	t.x = (480 - t.w) / 2;
-	t.y = 0;
-	t.margin = 14;
-	t.defaultStyle = {
+	const current = new TextArea(480, 90);
+	current.x = (480 - current.w) / 2;
+	current.y = 0;
+	current.margin = 14;
+	current.defaultStyle = {
 		color: '#fff',
 		size: '36',
 		family: 'PixelMplus, sans-serif',
 		weight: 'bold',
-		align: 'center',
-		space: 0,
-		ruby: null,
-		rubyId: null
+		align: 'left',
+		space: 0
 	};
-	Hack.menuGroup.addChild(t);
+	Hack.menuGroup.addChild(current);
 	
 	let odai = getOdai();
-	
-	t.push(odai.join(kugiri));
-	t.show();
-	
-	window.addEventListener('keydown', e => {
+	show(current, odai.join(kugiri));
+	window.addEventListener('keydown', async e => {
+		if (!Hack.isPlaying) {
+			return;
+		}
 		if (e.key.toUpperCase() === odai[0]) {
+			Hack.score += 1;			
 			odai.shift();
+			show(current, odai.join(kugiri));
 			if (odai.length === 0) {
-				count++;
+				player.attack();
 				odai = getOdai();
+				show(current, odai.join(kugiri));
 			}
-			t.clear();
-			t.push(odai.join(kugiri));
-			t.show();
 		}
 	});
 	
 	function getOdai() {
-		const level = Math.min( count / 5 >> 0, odaihyou.length - 1);
-		const kouho = odaihyou[level];
+		const lv = Math.min(level(), odaihyou.length - 1);
+		const kouho = odaihyou[lv];
 		let odai = [];
-		for (let index = 0; index < mozisu; index++) {
+		for (let index = mozisu(); index > 0; index--) {
 			const number = Math.random() * kouho.length >> 0;
 			odai.push(kouho[number]);
 		}
 		return odai;
 	}
 	
+	function show(textArea, text) {
+		textArea.source = text;
+		textArea.updateDocument();
+		textArea.updateValues();
+		textArea.show();
+	}
+	
+	async function koumori() {
+		await player.wait(2);
+		// ここからコウモリ
+		const item1 = new RPGObject(('▼ スキン', Skin.コウモリ));
+		item1.family = ('▼ ファミリー', Family.ドクリツ);
+		item1.hp = hp();
+		item1.atk = 1;
+		item1.locate(11, 5, 'map1');
+		item1.on(('▼ イベント', 'たおれたとき'), async () => {
+			koumori();
+		});
+		item1.endless(async(self, count) => {
+			const ターゲット = player;
+			const moveX = 32 * Math.sign(ターゲット.mapX - self.mapX);
+			const moveY = 32 * Math.sign(ターゲット.mapY - self.mapY);
+			self.forward = [moveX, moveY];
+			await self.walk(); // あるく
+			await self.attack(); // こうげきする
+			await self.wait(1); // やすむ
+			/*+ じどう*/
+		});
+		// ここまでコウモリ
+	}
+	koumori();
+	
 	/*+ モンスター アイテム せっち システム */
 	
+	// ビーム
+	player.on(('▼ イベント', 'こうげきするとき'), (event) => {
+		const 使い手 = event.target;
+		const ビーム = 使い手.summon(('▼ スキン', Skin.ビーム));
+		ビーム.mod(Hack.createDamageMod(使い手.atk)); // ダメージオブジェクトにする
+		使い手.shoot(ビーム, 使い手.forward, 10);
+		ビーム.on('triggerexit', () => {
+			ビーム.destroy(); // かんつうしない
+		});
+	});
+	
 	/*+ スキル */
+	
+	Hack.on('gameclear', () => {
+		show(current, `スコアは${Hack.score}です`);
+	});
+	Hack.on('gameover', () => {
+		show(current, `スコアは${Hack.score}です`);		
+	});
 	
 	game._debug = ('▼ フラグ', false);
 }
