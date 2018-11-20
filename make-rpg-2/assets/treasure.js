6f8c9ba8c4d0ac49bf2dbe5e297541b047d9d6fb
teramotodaiki <i101323@gmail.com>
2018-11-20 13:30:08 +0900

    fix: こうげきされたときのバグ修正と、宝箱が一度しか開かないように

--- make-rpg-2/assets/treasure.js
+++ make-rpg-2/assets/treasure.js
@@ -4,14 +4,16 @@ rule.this = 'たからばこ';
 
 rule.つくられたとき(async function() {
 	this.mod(('▼ スキン', Skin.トレジャーボックス)); // 見た目をかえる
+	this.あいた = false; // まだ、あいていない（変数）
 	/*+ つくられたとき */
 });
 
 rule.item = 'プレイヤー';
 rule.こうげきされたとき(async function(item) {
-	// こうげきされたときにはバグがあって, 一度の攻撃でなんども呼ばれてしまう.修正予定
+	if (this.あいた) return; // もし、あいていたら、ここで終わる（変数）
 	this.mod(('▼ スキン', Skin.トレジャーボックス2)); // 見た目をかえる
 	const item1 = rule.つくる('コイン'); // コインを出す
 	item1.locate(this.mapX, this.mapY + 1); // 自分よりひとつ下におく
+	this.あいた = true; // あいた（変数）
 	/*+ こうげきされたとき */
 });
