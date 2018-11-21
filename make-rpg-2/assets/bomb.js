3b0eb1ea0f8df1e13ea824cccd36fa684ba53e57
teramotodaiki <i101323@gmail.com>
2018-11-21 18:45:20 +0900

    fix: しょうかん時に同じ場所に出る仕様に対応

--- make-rpg-2/assets/bomb.js
+++ make-rpg-2/assets/bomb.js
@@ -8,6 +8,7 @@ rule.つくられたとき(async function() {
 	await this.wait(3); // この秒数だけまつ
 
 	const item1 = this.しょうかんする('ばくはつ'); // ばくはつ を おこす
+	item1.locate(this.mapX, this.mapY); // ばくだんと同じ場所にする
 	item1.atk = this.atk;
 	this.destroy(); // ばくだん を けす
 	/*+ つくられたとき */
