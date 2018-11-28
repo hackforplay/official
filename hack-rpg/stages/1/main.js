2550dab1d69361b5f69fbbd86e816bb31ab949bf
teramotodaiki <i101323@gmail.com>
2018-11-28 23:11:22 +0900

    feature: earlybird のコアのバージョンを 0.10.x にする

--- hack-rpg/stages/1/main.js
+++ hack-rpg/stages/1/main.js
@@ -1,4 +1,4 @@
-import { enchant, Hack, register } from 'http://localhost:8080/main.js';
+import { enchant, Hack, register } from 'https://unpkg.com/@hackforplay/common@^0.10';
 
 import extra from '../extra';
 import createMap from './maps';
