715dc5970763ae99f0c29664fbb6856e9170df9a
teramotodaiki <i101323@gmail.com>
2018-11-29 23:06:24 +0900

    feature: ドラゴンは攻撃しかえす

--- make-rpg-2/assets/dragon.js
+++ make-rpg-2/assets/dragon.js
@@ -6,14 +6,17 @@ rule.つくられたとき(async function() {
 	this.mod(('▼ スキン', Skin.ドラゴン)); // 見た目をきめる
 	this.family = ('▼ ファミリー', Family.ドクリツ); // ファミリーを決める
 	this.hp = 10; // 体力を決める
-	this.atk = 1; // こうげき力を決める
+	this.atk = 3; // こうげき力を決める
 	/*+ つくられたとき */
 });
 
 rule.つねに(async function() {
-	await this.attack(); // こうげきする
-	await this.wait(1); // やすむ
 	/*+ つねに */
+});
+
+rule.こうげきされたとき(async function() {
+	await this.attack(); // こうげきしかえす
+	/*+ こうげきされたとき */
 });
 
 rule.たおされたとき(async function() {
