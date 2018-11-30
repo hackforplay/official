a936b0734dc31f4f85864b6819cb65fb81d6fa54
teramotodaiki <i101323@gmail.com>
2018-11-30 09:08:02 +0900

    fix

--- make-rpg-2/game.js
+++ make-rpg-2/game.js
@@ -2,6 +2,8 @@ import 'https://unpkg.com/@hackforplay/common@^0.10/dist/register.js';
 
 // ここからゲームがはじまったときのルール
 rule.ゲームがはじまったとき(async function() {
+	// マップ（β版）のコードは、この下に貼り付ける
+	
 	Hack.changeMap('map1'); // map1 をロード
 	
 	const player = rule.つくる('プレイヤー', 3, 5, 'map1', ('▼ むき', Dir.した));
