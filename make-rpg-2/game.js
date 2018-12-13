9b152d87946e39430c8a2f016afc5ba282a6d23b
teramotodaiki <i101323@gmail.com>
2018-12-13 16:33:55 +0900

    feature: scope を修正, fold の指定を除去

--- make-rpg-2/game.js
+++ make-rpg-2/game.js
@@ -1,6 +1,5 @@
 import '__FEELES_COMMON_REGISTER__';
 
-// ここからゲームがはじまったときのルール
 rule.ゲームがはじまったとき(async function() {
 	// マップ（β版）のコードは、この下に貼り付ける
 	
@@ -9,6 +8,6 @@ rule.ゲームがはじまったとき(async function() {
 	const player = rule.つくる('プレイヤー', 3, 5, 'map1', ('▼ むき', Dir.した));
 	window.player = player;
 	
-	/*+ キャラクター アイテム */
+	
+	/*+ ゲームがはじまったとき */
 });
-// ここまでゲームがはじまったときのルール
