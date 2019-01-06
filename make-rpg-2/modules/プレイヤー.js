ae99f1047e734f7d283738d62d0c8ae91ad9a8f5
teramotodaiki <i101323@gmail.com>
2019-01-06 11:49:09 +0900

    feature: skin から costume に変更, スキンのドロップダウンを追加

--- make-rpg-2/modules/プレイヤー.js
+++ make-rpg-2/modules/プレイヤー.js
@@ -4,17 +4,20 @@ rule.this = 'プレイヤー'
 
 rule.つくられたとき(async function() {
 	Player.set(this)
-	this.skin = Hack.skin('ゆうしゃ男') // 見た目をきめる
-	this.family = ('▼ なかま', Family.プレイヤー) // だれと仲間か
+	await this.costume(('▼ みため', 'ゆうしゃ男'))
+	
+	this.family = ('▼ なかま', Family.プレイヤー)
+	
 	this.n('たいりょく', ('▼ を', 'イコール'), 3)
+	
 	this.n('こうげきりょく', ('▼ を', 'イコール'), 1)
-
+	
 	/*+ つくられたとき */
 })
 
 rule.たおされたとき(async function() {
 	Hack.gameover() // ゲームオーバー
 	this.destroy() // プレイヤーを消す
-
+	
 	/*+ たおされたとき */
 })
