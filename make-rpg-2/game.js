b13c296893c6e508ab0a6bd5d18e8092e7052555
teramotodaiki <i101323@gmail.com>
2019-01-19 17:13:36 +0900

    feat: 0.13.0 に対応

--- make-rpg-2/game.js
+++ make-rpg-2/game.js
@@ -3,7 +3,7 @@ import '__FEELES_COMMON_REGISTER__';
 rule.ゲームがはじまったとき(async function() {
 	// マップ（β版）のコードは、この下に貼り付ける
 	
-	Hack.changeMap('map1'); // map1 をロード
+	await Hack.changeMap('map1'); // map1 をロード
 	
 	const player = rule.つくる('プレイヤー', 3, 5, 'map1', ('▼ むき', Dir.した));
 	window.player = player;
