b59b78f42d29540525a89e1ffbacbaa2002c07c0
teramotodaiki <i101323@gmail.com>
2019-02-05 12:15:32 +0900

    feat: env.mjs で common パッケージの参照先を変えられるように

---
+++ env.mjs
@@ -0,0 +1,7 @@
+// 本番環境と同じにする（インターネットアクセスが必要）
+export const __FEELES_COMMON_INDEX__ = 'https://unpkg.com/@hackforplay/common@^0.10' // hack-rpg, make-rpg
+export const __FEELES_COMMON_REGISTER__ = 'https://unpkg.com/@hackforplay/common@^0.13/dist/register.js' // make-rpg-2
+
+// localhost の common を取得
+// export const __FEELES_COMMON_REGISTER__ = 'http://localhost:8080/register.js' // hack-rpg, make-rpg
+// export const __FEELES_COMMON_INDEX__ = 'http://localhost:8080/main.js' // make-rpg-2
