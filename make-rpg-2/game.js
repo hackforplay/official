589216833d4a7b4237cf8a58b2304b1a9cc49183
teramotodaiki <i101323@gmail.com>
2019-05-24 12:15:05 +0900

    fix: 古いマップの指定方法を削除

--- make-rpg-2/game.js
+++ make-rpg-2/game.js
@@ -1,8 +1,6 @@
 import '__FEELES_COMMON_REGISTER__';
 
 rule.ゲームがはじまったとき(async function() {
-	// マップ（β版）のコードは、この下に貼り付ける
-	
 	await Hack.changeMap('map1'); // map1 をロード
 	
 	const player = rule.つくる('プレイヤー', 3, 5, 'map1', ('▼ むき', Dir.した));
