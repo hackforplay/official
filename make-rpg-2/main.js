a5fe1c32b0878136857a10468d6f34b49930795d
teramotodaiki <i101323@gmail.com>
2019-09-06 12:11:07 +0900

    fix: 旧マップ定義を削除

--- make-rpg-2/main.js
+++ make-rpg-2/main.js
@@ -1,5 +1,4 @@
 import './game' // ルール定義をロードする
-import maps from './maps'
 import './autoload' // モジュールのオートロード
 
 // ゲームをつくる
@@ -32,27 +31,7 @@ game.onload = async () => {
 // マップをつくる
 Hack.onload = async () => {
 	Hack.maps = Hack.maps || {}
-	try {
-		// GUI で切り替え可能なマップ (common~0.12)
-		await Hack.loadMaps('maps.json')
-	} catch (error) {
-		console.error(error)
-		// 後方互換性を残す
-		maps()
-		// マップエディタが完成するまでのデフォルトマップ
-		await Hack.loadMap(
-			'map1',
-			'https://storage.googleapis.com/hackforplay-ugc/e6ac1ed6-9b50-4429-8c79-46253acb7449.json'
-		)
-		await Hack.loadMap(
-			'map2',
-			'https://storage.googleapis.com/hackforplay-ugc/a0edeac0-7171-4144-90f2-affb34683d38.json'
-		)
-		await Hack.loadMap(
-			'map3',
-			'https://storage.googleapis.com/hackforplay-ugc/ece88d4e-7f25-463d-b4d2-b03e6867ab83.json'
-		)
-	}
+	await Hack.loadMaps('maps.json')
 }
 
 // ゲームスタート
